7f98128efb119a2145565d4baf8b905b9a4aa5b6
Benjamin Gilbert <benjamin.g.gilbert97@gmail.com>
2019-04-08 06:09:17 +0000

    encryption/decryption template

--- main.c
+++ main.c
@@ -4,8 +4,37 @@ void rotate (int k, int e);
 
 int main (){
     
-    char str [20] = {"ATTACK AT SUNRISE \0"};  
-    printf ("%s\n", str);
+    int i, x;
+    
+    char str [100];  
+    
+    printf ("\n Please enter a string: \t");
+    gets(str);
+    
+    printf ("\n Please choose following options: \n");
+    printf ("1 = Encrypt the string. \n");
+    printf ("2 = Decrypt the string. \n");
+    scanf ("%d", &x);
+    
+    switch (x)
+    {
+        case 1:
+            for (i = 0; (i < 100 && str[i] != '\0'); i++)
+                str[i] = str[i] + 3;
+                
+                printf ("\nEncrypted string: %s\n", str);
+            break;
+                
+        case 2:
+            for (i = 0; (i < 100 && str[i] != '\0'); i++)
+                str[i] = str[i] - 3;
+            
+                printf ("\nDecrypted string: %s\n", str);
+            break;
+            
+        default:
+            printf ("\nError\n");
+    }
     
   return 0;
   
