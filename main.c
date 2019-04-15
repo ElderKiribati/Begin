0c4cacfd5b2dc00191940fa656361ad34f52b10e
Benjamin Gilbert <benjamin.g.gilbert97@gmail.com>
2019-04-15 01:22:48 +0000

    still trying

--- main.c
+++ main.c
@@ -1,77 +1,34 @@
 #include <stdio.h>
 
-char rotate ();
-char restore ();
+char rotate (char x);
+char restore (char x);
 
-int main(){
-   char first, second;
-   int x;
-   
+int main()
+{
+   char c;
+
    FILE *input;
    
-   input = fopen("input.txt", "r");
+   input = fopen ("input.txt", "r");
    
-   printf ("%c", input);
-   scan ("%c", &input);
-
-   printf ("\nPlease choose following options:\n");
-   printf ("1 = Encrypt the string.\n");
-   printf ("2 = Decrypt the string.\n");
-   scanf ("%d", &x);
-   
-  
-
-   //using switch case statements
-   switch(x){
-   case 1:
-        first = rotate ();
-    printf ("\nEncrypted string: %s\n", first);
-    break;
-
-   case 2:
-        second = restore();
-      printf ("\nDecrypted string: %s\n", second);
-      break;
-
-   default:
-      printf ("\nError\n");
-   }
-   
-   
-   printf ("\nPlease choose following options:\n");
-   printf ("1 = Encrypt the string.\n");
-   printf ("2 = Decrypt the string.\n");
-   scanf ("%d", &x);
-   
-   switch(x){
-   case 1:
-        first = rotate ();
-    printf ("\nEncrypted string: %s\n", first);
-    break;
-
-   case 2:
-        second = restore();
-      printf ("\nDecrypted string: %s\n", second);
-      break;
-
-   default:
-      printf ("\nError\n");
+   while (feof (input) == 0){
+       fscanf (input, "%c", &c);
+       rotate (&c);
+       printf ("%c", c);       
    }
    return 0;
 }
-
-char rotate (){
+char rotate (char x){
     int i;
-    char message[1024];
-        for (i = 0; (i < 100 && message[i] != NULL); i++)
-            message[i] = message[i] + 3; //the key for encryption is 3 that is added to ASCII value
-        return message[i];
+    for(i = 0; (i < 100 && str[i] != '\0'); i++)
+        str[i] = str[i] + 3; //the key for encryption is 3 that is added to ASCII value
+    return str[i];
 }
 
-char restore (){
-    int a;
-    char message[1024];
-        for (a = 0; (a < 100 && message[a] != NULL); a++)
-            message[a] = message[a] - 3; //the key for encryption is 3 that is subtracted to ASCII value
-        return message[a];
+char restore (char x){
+    int i;
+    char str[100];
+    for(i = 0; (i < 100 && str[i] != '\0'); i++)
+        str[i] = str[i] + 3; //the key for encryption is 3 that is added to ASCII value
+    return str[i];
 }
