c72561c6d3c015cf18a401fe01f778020559b2e3
Benjamin Gilbert <benjamin.g.gilbert97@gmail.com>
2019-04-23 04:13:30 +0000

    almost there

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
-//char restore ();
-
 int main(){
-    char str[] = "hello";//message to be encrypted
+    char str[1024];//message to be encrypted
+    char str1[1024];// message to be decrypted
+    int a;
     int i, x;
     
+    FILE *input;
+    
+    input = fopen ("input.txt", "r");
+    
+    while (feof (input) == 0){//reads message from file to be encrypted
+        fscanf (input, "%s", &str);
+    }
     
     printf ("\nChoose from following options: \n");
     printf ("1 = encryprtion. \n");
@@ -13,49 +20,39 @@ int main(){
     scanf ("%d", &x);
     
     switch (x){
+        
         case 1: //when '1' is entered, 3 is added to the ASCII value
             for (i = 0; (i <100 && str[i] != '\0'); i++)
                 str[i] = str[i] + 3;
             printf ("%s\n", str);
             
-            /*//this take the encrypted message and stores it in a file.
+            //this takes the encrypted message and stores it in a file.
             FILE *output;
              
-            
             output = fopen ("output.txt", "w");
             
-            while (feof (output) != '\0'){
-                fprintf (output, "%s", str);
+            while (feof (output) == 0){
+                fprintf (output, "%s", &str);
             }
-            fclose (output);*/
+            fclose (output);
+            break;
             
+        case 2:/*when '2' is selected the output file containing
+                the encrypted message is opened and then decrypted.*/
+            FILE *output;
+    
+            output = fopen ("output.txt", "r");
+    
+            while (feof (output) == 0){
+                fscanf (output, "%s", str1);
+            }
+            for (a = 0; (a < 100 && str1[a] != '\0'); a++)
+                str1[a] = str1[a] - 3;
+                printf ("%s", str1);        
             break;
-        case 2://when '2' is selected, 3 is subtracted from the ASCII value.
-            for (i = 0; (i <100 && str[i] != '\0'); i++)
-                str[i] = str[i] - 3;
-            printf ("%s\n", str);
-            //restore ();        
-            break;
+            
         default:
             printf ("\nERROR, invalid input");
     }
     return 0;
 }
-
-/*//this function reads the file with the encrypted message and decrypts it.
-char restore (){
- 
-    char r[1024];
-    int a;
-    FILE *output;
-    
-    output = fopen ("output.txt", "r");
-    
-    while (feof (output) != '\0'){
-        fscanf (output, "%s", r);
-    }
-    for (a = 0; (a < 100 && r[a] != '\0'); a++)
-        r[a] = r[a] - 3;
-        printf ("%s", r);
-    return 0;
-}*/
